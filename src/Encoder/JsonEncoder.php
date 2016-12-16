ec51401a7ac6303d894f5167c4c518ff154cb21b
e0ipso <e0ipso@550110.no-reply.drupal.org>
2016-12-16 22:39:18 +0100

    docs(Serialization) Clean up \Drupal\jsonapi\Encoder\JsonEncoder (#2829407 by e0ipso, Wim Leers)

--- src/Encoder/JsonEncoder.php
+++ src/Encoder/JsonEncoder.php
@@ -38,10 +38,21 @@ class JsonEncoder extends SymfonyJsonEncoder {
     return $format == $this->format;
   }
 
+  /**
+   * {@inheritdoc}
+   *
+   * @see http://jsonapi.org/format/#errors
+   */
   public function encode($data, $format, array $context = []) {
+    // Make sure that any auto-normalizable object gets normalized before
+    // encoding. This is specially important to generate the errors in partial
+    // success responses.
     if ($data instanceof ValueExtractorInterface) {
       $data = $data->rasterizeValue();
     }
+    // Allows wrapping the encoded output. This is so we can use the same
+    // encoder and normalizers when serializing HttpExceptions to match the
+    // JSON API specification.
     if (!empty($context['data_wrapper'])) {
       $data = [$context['data_wrapper'] => $data];
     }
