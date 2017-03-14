21cb12feb6606cc77d43e1dc10fa7fa9f79da7d3
martin107 <martin107@2659379.no-reply.drupal.org>
2017-03-14 07:39:16 +0100

    cleanup(Documentation): UnprocessableHttpEntityException: Mismatch between annonation and constructor definition (#2852821 by martin107, gaurav.kapoor)

--- src/Exception/UnprocessableHttpEntityException.php
+++ src/Exception/UnprocessableHttpEntityException.php
@@ -7,6 +7,14 @@ use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
+ * A class to represent a 422 - Unprocessable Entity Exception.
+ *
+ * The HTTP 422 status code is used when the server sees:-
+ *
+ *  The content type of the request is correct.
+ *  The syntax of the request is correct.
+ *  BUT was unable to process the contained instruction.
+ *
  * @internal
  */
 class UnprocessableHttpEntityException extends HttpException {
@@ -23,10 +31,12 @@ class UnprocessableHttpEntityException extends HttpException {
   /**
    * UnprocessableHttpEntityException constructor.
    *
-   * @param array $violations
    * @param \Exception|null $previous
+   *    The pervious error, if any, associated with the request.
    * @param array $headers
+   *    The headers associated with the request.
    * @param int $code
+   *    The HTTP status code associated with the request. Defaults to zero
    */
   public function __construct(\Exception $previous = NULL, array $headers = array(), $code = 0) {
     parent::__construct(422, "Unprocessable Entity: validation failed.", $previous, $headers, $code);
@@ -36,6 +46,7 @@ class UnprocessableHttpEntityException extends HttpException {
    * Gets the constraint violations associated with this exception.
    *
    * @return \Drupal\Core\Entity\EntityConstraintViolationListInterface
+   *    The constraint violations.
    */
   public function getViolations() {
     return $this->violations;
