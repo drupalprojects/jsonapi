10a82e5030ef51dc9ba77761224ec9cf4ddd55dd
gabesullice <gabesullice@2287430.no-reply.drupal.org>
2018-05-15 18:55:31 +0200

    Issue #2932679 by gabesullice, Wim Leers: Remove unused "on_relationship" serialization context

--- src/EventSubscriber/ResourceResponseSubscriber.php
+++ src/EventSubscriber/ResourceResponseSubscriber.php
@@ -137,7 +137,6 @@ class ResourceResponseSubscriber implements EventSubscriberInterface {
           'request' => $request,
           'cacheable_metadata' => $response->getCacheableMetadata(),
           'resource_type' => $request->get('resource_type'),
-          'on_relationship' => (bool) $request->get('_on_relationship'),
         ]);
       };
       $output = $this->renderer->executeInRenderContext($context, $render_function);
